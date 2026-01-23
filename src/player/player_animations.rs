17a72f3321de79b3a45d2e2a4a813372daabf25b
Tymophiy Radchenko <timaradchen@gmail.com>
2026-01-23 08:46:42 +0100

    finnaly animationg hand!

--- src/player/player_animations.rs
+++ src/player/player_animations.rs
@@ -4,19 +4,26 @@ pub fn animate_kick( mut commands: Commands,
     time: Res<Time>,
     mut query: Query<(Entity,&mut Transform, &mut KickAnimation)>,
 ) {
-    // for (entity, mut transform, mut anim) in query.iter_mut() {
-    //     anim.progress += time.delta_secs() / anim.duration;
-        
-    //     anim.progress = anim.progress.clamp(0.0, 1.0);
-        
-    //     let angle = swing_angle(anim.progress, anim.max_angle);
-        
-    //     transform.rotation = Quat::from_rotation_z(angle);
-        
-    //     if anim.progress >= 1.0 {
-    //         anim.active = false; 
-    //     } 
-    // } 
+    for (entity, mut transform, mut anim) in query.iter_mut() {
+        if anim.active {
+            println!("kicking!");
+            anim.progress += time.delta_secs() / anim.duration;
+            
+            anim.progress = anim.progress.clamp(0.0, 1.0);
+            println!("stinking!");
+            let angle = swing_angle(anim.progress, anim.max_angle);
+            
+            transform.rotation = Quat::from_rotation_z(angle);
+            println!("fooling!");
+            
+            if anim.progress >= 1.0 {
+                anim.active = false;
+                anim.progress = 0.0;
+                anim.duration = 0.0;
+                println!("resetting!");
+            } 
+        }
+    } 
 } 
 
 
@@ -32,5 +39,19 @@ t * max_angle_rad
 }
 
 pub fn start_kick(
-    
-) {}
+    mut hand: Query<&mut KickAnimation, With<HeldItem>>,
+    mut reader: MessageReader<MouseClickEvent>,
+) { 
+    for msg in reader.read() {
+        if let Ok(mut anim) = hand.single_mut() {
+            println!("found animation!");
+            if anim.active {
+                continue;
+            } else {
+                anim.active = true;
+                anim.duration = 0.2;
+                println!("Kick animation started");
+            }
+        }
+    }
+}
