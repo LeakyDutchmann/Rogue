d55845cfc6ef465d073435bf6c53296f323702ec
Tymophiy Radchenko <timaradchen@gmail.com>
2026-01-23 08:48:37 +0100

    a bit of clearing.

--- src/player/player_animations.rs
+++ src/player/player_animations.rs
@@ -6,21 +6,18 @@ pub fn animate_kick( mut commands: Commands,
 ) {
     for (entity, mut transform, mut anim) in query.iter_mut() {
         if anim.active {
-            println!("kicking!");
+
             anim.progress += time.delta_secs() / anim.duration;
             
             anim.progress = anim.progress.clamp(0.0, 1.0);
-            println!("stinking!");
             let angle = swing_angle(anim.progress, anim.max_angle);
             
             transform.rotation = Quat::from_rotation_z(angle);
-            println!("fooling!");
             
             if anim.progress >= 1.0 {
                 anim.active = false;
                 anim.progress = 0.0;
                 anim.duration = 0.0;
-                println!("resetting!");
             } 
         }
     } 
@@ -44,13 +41,11 @@ pub fn start_kick(
 ) { 
     for msg in reader.read() {
         if let Ok(mut anim) = hand.single_mut() {
-            println!("found animation!");
             if anim.active {
                 continue;
             } else {
                 anim.active = true;
                 anim.duration = 0.2;
-                println!("Kick animation started");
             }
         }
     }
