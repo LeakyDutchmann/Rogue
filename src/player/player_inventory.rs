7d9c25061486ab40af51e6efeb936cceb3edd74f
Tymophiy Radchenko <timaradchen@gmail.com>
2026-03-18 10:55:08 +0100

    added item counter!

--- src/player/player_inventory.rs
+++ src/player/player_inventory.rs
@@ -36,6 +36,15 @@ pub fn setup_inventory(
                     index: i,
                 },
             ));
+            parent.spawn((
+                Text::new(""),
+                Node {
+                    position_type: PositionType::Absolute,
+                    top: Val::Percent((50.0)),
+                    ..Default::default()
+                },
+                SlotCounter,
+            ));
         }).id());
     }
     let hotbar = commands.spawn((
@@ -491,6 +500,37 @@ pub fn ui_cursor_update(
                 }
             } else if cursor.item.is_none() {
                 commands.entity(entity).remove::<ImageNode>();
+            }
+        }
+    }
+}
+
+pub fn update_item_count(
+    mut slots: Query<(&mut Text, &ChildOf), With<SlotCounter>>,
+    children: Query<&Children>,
+    slot_idx: Query<&SlotIcon>,
+    inventory: Query<&Inventory, With<Player>>,
+) {
+    let mut slot_items: Vec<(usize, i32)> = Vec::new();
+    if let Ok(inv) = inventory.single() {
+        for (i, item) in inv.items.iter().enumerate() {
+            slot_items.push((i, item.quantity));
+        }
+    }
+    for (mut text, parent) in slots.iter_mut() {
+        if let Ok(children) = children.get(parent.0) {
+            for child in children {
+                if let Ok(idx) = slot_idx.get(*child) {
+                    for slot in &slot_items {
+                        if slot.0 == idx.index {
+                            if slot.1 == 0 || slot.1 == 1 {
+                                text.0 = "".to_string();
+                            } else {
+                                text.0 = slot.1.to_string();
+                            }
+                        }
+                    }
+                }
             }
         }
     }
